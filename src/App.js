03ada0480e494e3d367371fadafe97d268d22431
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-05 19:55:17 +0300

    add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Navbar from './components/Navbar';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100vh;
+`;
+
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <Container>
+            <Navbar />
+        </Container>
+    );
 }
 
 export default App;
